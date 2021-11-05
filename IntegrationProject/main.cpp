3f9bc4fddb9d219e364847662c795f879538d77e
Nick <62121599+nick-toon@users.noreply.github.com>
2021-11-04 22:10:56 -0400

    Add files via upload

---
+++ IntegrationProject/main.cpp
@@ -0,0 +1,21 @@
+/* Nicholas Cotugno
+ * Compare/Contrast of procededural/functional programing vs OOP
+ * In procedural/functional programming, programs are divided
+ * into functions while OOP is divided into objects.
+ * Procedural/functional is top down and OOP is bottom up
+ * OOP uses access specifiers like public, private, and protected.Procedural/functional does not.
+ * OOP can use overloading, procedural/functional can not.
+*/
+
+#include "mainwindow.h"
+#include <QApplication>
+#include <QLabel>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    MainWindow w;
+    w.setWindowTitle("Integration Project");
+    w.show();
+    return a.exec();
+}

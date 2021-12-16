6ecc030355480572e575c1f4d88fc15cf099bd04
Nick <62121599+nick-toon@users.noreply.github.com>
2021-12-15 20:00:50 -0500

    Add files via upload

--- IntegrationProjectPart2/main.cpp
+++ IntegrationProjectPart2/main.cpp
@@ -5,9 +5,17 @@
  *  mainwindow including the notepad application. It also sets the
  *  title of the window to 'NotePadZ' shown on line 19.
  *
+ * LO4: Compare/Contrast of procededural/functional programing vs OOP
+ * In procedural/functional programming, programs are divided
+ * into functions while OOP is divided into objects.
+ * Procedural/functional is top down and OOP is bottom up
+ * OOP uses access specifiers like public, private, and protected.Procedural/functional does not.
+ * OOP can use overloading, procedural/functional can not.
+ *
  *  @author Nicholas Cotugno
  *  @bug No known bugs.
  */
+ 
 #include "mainwindow.h"
 
 #include <QApplication>
